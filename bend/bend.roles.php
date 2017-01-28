92347aa81688012d4341bc7186a98cca7a80e918
Carsten Eckelmann <careck@gmail.com>
2017-01-28 14:02:07 +1100

    fixed roles layout

--- bend/bend.roles.php
+++ bend/bend.roles.php
@@ -6,11 +6,14 @@ function role_bend_admin_allowed(Web $w, $path) {
 
 function role_bend_user_allowed(Web $w, $path) {
     return 
-    	$w->checkUrl($path, "bend", "workhours", 
-    			["index","editworkentry","enter",
-    				"ajax_getchildcategories",
-    				"ajax_gethouseholds","list"]) || 
-    	$w->checkUrl($path, "bend", "electricity", 
-    			["index"]) ||
+    	$w->checkUrl($path, "bend", "workhours", [
+    			"index",
+    			"editworkentry",
+    			"enter",
+    			"ajax_getchildcategories",
+    			"ajax_gethouseholds",
+    			"list"]) || 
+    	$w->checkUrl($path, "bend", "electricity", [
+    			"index"]) ||
     	$w->checkUrl($path, "bend", "", "index");
 }
